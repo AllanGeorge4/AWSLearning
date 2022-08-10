ce92932df600bb3d16faf66808aa4f08fc877d1b
allan_george <allangeorge12w@gmail.com>
2022-08-10 10:31:42 +0530

    First Commit

---
+++ src/main/java/com/bourntec/VisaProject/VisaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bourntec.VisaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bourntec/VisaProject/VisaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bourntec.VisaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
